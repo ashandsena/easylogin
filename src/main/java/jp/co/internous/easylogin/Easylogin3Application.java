38a694fc041911562f82faefb45f0d685a960d80
ashandsena <ash-andsena@outlook.jp>
2024-08-29 21:40:15 +0900

    Lesson_Easylogin

---
+++ src/main/java/jp/co/internous/easylogin/Easylogin3Application.java
@@ -0,0 +1,13 @@
+package jp.co.internous.easylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Easylogin3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Easylogin3Application.class, args);
+	}
+
+}
